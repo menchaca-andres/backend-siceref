e50cfcde7f0f73f601ebcd59356c72619f987d76
menchaca-andres <4mrvro@gmail.com>
2026-04-23 22:35:43 -0400

    importación de todas las rutas de los modulos

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import roleRoutes from './modules/roles/role.routes'
+import especieRoutes from './modules/especies/especie.routes'
+import razaRoutes from './modules/razas/raza.routes'
+import usuarioRoutes from './modules/usuarios/usuario.routes'
+import mascotaRoutes from './modules/mascotas/mascota.routes'
 
 const app = express()
-
 app.use(express.json())
 
+app.use('/api/roles', roleRoutes)
+app.use('/api/especies', especieRoutes)
+app.use('/api/razas', razaRoutes)
+app.use('/api/usuarios', usuarioRoutes)
+app.use('/api/mascotas', mascotaRoutes)
 
 export default app

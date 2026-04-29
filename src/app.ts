20dfc371dedbe69a80e1870af5624120abaaf178
menchaca-andres <4mrvro@gmail.com>
2026-04-29 09:13:33 -0400

    agg. cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import roleRoutes from './modules/roles/role.routes'
 import especieRoutes from './modules/especies/especie.routes'
 import razaRoutes from './modules/razas/raza.routes'
@@ -8,6 +9,13 @@ import refugioRoutes from './modules/refugios/refugio.routes'
 import authRoutes from './modules/auth/auth.routes'
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json())
 
 app.use('/api/roles', roleRoutes)

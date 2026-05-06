751440b655ae4768cb4df532b2a5ea4370523a44
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:48:54 -0400

    adaptando types en base a scheme.prisma

--- src/modules/roles/role.types.ts
+++ src/modules/roles/role.types.ts
@@ -1,12 +1,18 @@
 export interface Role {
     id_rol: number
+    codigo: string
     nom_rol: string
+    descrip_rol: string
 }
 
 export interface CreateRoleDto {
+    codigo: string
     nom_rol: string
+    descrip_rol: string
 }
 
 export interface UpdateRoleDto {
+    codigo?: string
     nom_rol?: string
+    descrip_rol?: string
 }

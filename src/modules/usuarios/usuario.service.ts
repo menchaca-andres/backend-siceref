c019f48861d71a2b77233074f3ac1e28704c9f4d
menchaca-andres <4mrvro@gmail.com>
2026-05-06 05:00:04 -0400

    verificando existencia del usuario y hasheo

--- src/modules/usuarios/usuario.service.ts
+++ src/modules/usuarios/usuario.service.ts
@@ -5,6 +5,11 @@ import bcrypt from 'bcrypt'
 export const UsuarioService = {
     getAll: async () => await UsuarioModel.findAll(),
 
+    getMyWorkers: async (id_ref: number | null) => {
+        if (id_ref === null) throw new Error('No perteneces a ningun refugio')
+        return await UsuarioModel.findWorkersByRefugio(id_ref)
+    },
+
     getById: async (id: number) => {
         const usuario = await UsuarioModel.findById(id)
         if (!usuario) throw new Error('Usuario no encontrado')
@@ -12,12 +17,12 @@ export const UsuarioService = {
     },
 
     create: async (data: CreateUsuarioDto) => {
-        const existe = await UsuarioModel.findByEmail(data.corr_usuario)
+        const existe = await UsuarioModel.findByEmail(data.email_usu)
         if (existe) throw new Error('El correo ya está registrado')
 
-        const hashPassword = await bcrypt.hash(data.contra_usuario, 10)
+        const hashPassword = await bcrypt.hash(data.pass_usu, 10)
 
-        return await UsuarioModel.create({ ...data, contra_usuario: hashPassword })
+        return await UsuarioModel.create({ ...data, pass_usu: hashPassword })
     },
 
     update: async (id: number, data: UpdateUsuarioDto) => {

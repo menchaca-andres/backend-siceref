129d2fef6301f89ad5e4e2670b4b16a6087edef8
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:41:57 -0400

    otorgando permisos por su codigo

--- src/modules/razas/raza.routes.ts
+++ src/modules/razas/raza.routes.ts
@@ -5,10 +5,10 @@ import { autorizar } from '../../middleware/roles.middleware'
 
 const router = Router()
 
-router.get('/', verificarToken, autorizar('Superadmin', 'Administrador Refugio', 'Trabajador Refugio'), RazaController.getAll)
-router.get('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio', 'Trabajador Refugio'), RazaController.getById)
-router.post('/', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), RazaController.create)
-router.put('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), RazaController.update)
-router.delete('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), RazaController.delete)
+router.get('/', verificarToken, autorizar('razas:obtener'), RazaController.getAll)
+router.get('/:id', verificarToken, autorizar('razas:obtener'), RazaController.getById)
+router.post('/', verificarToken, autorizar('razas:crear'), RazaController.create)
+router.put('/:id', verificarToken, autorizar('razas:modificar'), RazaController.update)
+router.delete('/:id', verificarToken, autorizar('razas:eliminar'), RazaController.delete)
 
 export default router

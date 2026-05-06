08c30c43085dc4225f5208984e50c31cc86de333
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:25:52 -0400

    adaptando a prisma eliminando pools

--- src/modules/especies/especie.model.ts
+++ src/modules/especies/especie.model.ts
@@ -1,38 +1,24 @@
-import { pool } from '../../config/database'
+import { prisma } from '../../config/database'
 import { CreateEspecieDto, UpdateEspecieDto } from './especie.types'
 
 export const EspecieModel = {
     findAll: async () => {
-        const result = await pool.query('SELECT * FROM ESPECIES')
-        return result.rows
+        return await prisma.especies.findMany({ orderBy: { id_esp: 'asc' } })
     },
 
     findById: async (id: number) => {
-        const result = await pool.query('SELECT * FROM ESPECIES WHERE id_espe = $1', [id])
-        return result.rows[0]
+        return await prisma.especies.findUnique({ where: { id_esp: id } })
     },
 
     create: async (data: CreateEspecieDto) => {
-        const result = await pool.query(
-            'INSERT INTO ESPECIES (nom_espe) VALUES ($1) RETURNING *',
-            [data.nom_espe]
-        )
-        return result.rows[0]
+        return await prisma.especies.create({ data })
     },
 
     update: async (id: number, data: UpdateEspecieDto) => {
-        const result = await pool.query(
-            'UPDATE ESPECIES SET nom_espe = $1 WHERE id_espe = $2 RETURNING *',
-            [data.nom_espe, id]
-        )
-        return result.rows[0]
+        return await prisma.especies.update({ where: { id_esp: id }, data }).catch(() => null)
     },
 
     delete: async (id: number) => {
-        const result = await pool.query(
-            'DELETE FROM ESPECIES WHERE id_espe = $1 RETURNING *',
-            [id]
-        )
-        return result.rows[0]
+        return await prisma.especies.delete({ where: { id_esp: id } }).catch(() => null)
     },
 }

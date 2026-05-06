960dd00a8c464f0e3f86bff230dde0a6cf9125fc
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:23:42 -0400

    otorgando permisos por su codigo

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -7,8 +7,8 @@ const router = Router()
 
 router.post('/login', AuthController.login)
 router.post('/register', AuthController.register)
-router.post('/register/worker', verificarToken, autorizar('Administrador Refugio'), AuthController.registerWorker)
-router.post('/register/superadmin', verificarToken, autorizar('Superadmin'), AuthController.registerSuperadmin)
-router.post('/register/admin-refugio', verificarToken, autorizar('Superadmin'), AuthController.registerAdminRefugio)
+router.post('/register/worker', verificarToken, autorizar('trabajadores:crear'), AuthController.registerWorker)
+router.post('/register/superadmin', verificarToken, autorizar('admins-sistema:crear'), AuthController.registerSuperadmin)
+router.post('/register/admin-refugio', verificarToken, autorizar('admins-refugio:crear'), AuthController.registerAdminRefugio)
 
 export default router

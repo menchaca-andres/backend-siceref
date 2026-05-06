0da783846b5928fe168e7e445f8e8f809955cc9a
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:28:48 -0400

    otorgando permisos por su codigo

--- src/modules/mascotas/mascota.routes.ts
+++ src/modules/mascotas/mascota.routes.ts
@@ -5,10 +5,10 @@ import { autorizar } from '../../middleware/roles.middleware'
 
 const router = Router()
 
-router.get('/', verificarToken, autorizar('Superadmin', 'Administrador Refugio', 'Trabajador Refugio'), MascotaController.getAll)
-router.get('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio', 'Trabajador Refugio'), MascotaController.getById)
-router.post('/', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), MascotaController.create)
-router.put('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), MascotaController.update)
-router.delete('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio'), MascotaController.delete)
+router.get('/', verificarToken, autorizar('mascotas:obtener'), MascotaController.getAll)
+router.get('/:id', verificarToken, autorizar('mascotas:obtener'), MascotaController.getById)
+router.post('/', verificarToken, autorizar('mascotas:crear'), MascotaController.create)
+router.put('/:id', verificarToken, autorizar('mascotas:modificar'), MascotaController.update)
+router.delete('/:id', verificarToken, autorizar('mascotas:eliminar'), MascotaController.delete)
 
 export default router

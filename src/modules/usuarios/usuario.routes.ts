b8f57c60eb202edb1d0b2b2beb9cd37419f4f5d8
menchaca-andres <4mrvro@gmail.com>
2026-05-06 05:01:22 -0400

    otorgando permisos por su codigo

--- src/modules/usuarios/usuario.routes.ts
+++ src/modules/usuarios/usuario.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import { UsuarioController } from './usuario.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
-import { autorizar } from '../../middleware/roles.middleware'
+import { autorizar, autorizarAlguno, autorizarPerfilPropioOPermiso } from '../../middleware/roles.middleware'
 
 const router = Router()
 
-router.get('/', verificarToken, autorizar('Superadmin'), UsuarioController.getAll)
-router.get('/:id', verificarToken, autorizar('Superadmin', 'Administrador Refugio', 'Trabajador Refugio', 'Adoptante'), UsuarioController.getById)
-router.post('/', verificarToken, autorizar('Superadmin'), UsuarioController.create)
-router.put('/:id', verificarToken, autorizar('Superadmin', 'Adoptante'), UsuarioController.update)
-router.delete('/:id', verificarToken, autorizar('Superadmin'), UsuarioController.delete)
+router.get('/', verificarToken, autorizar('usuarios:obtener'), UsuarioController.getAll)
+router.get('/trabajadores/mis-trabajadores', verificarToken, autorizar('trabajadores:obtener'), UsuarioController.getMyWorkers)
+router.get('/:id', verificarToken, autorizarAlguno('perfil:obtener', 'usuarios:obtener'), autorizarPerfilPropioOPermiso('usuarios:obtener'), UsuarioController.getById)
+router.post('/', verificarToken, autorizar('usuarios:crear'), UsuarioController.create)
+router.put('/:id', verificarToken, autorizarAlguno('perfil:modificar', 'usuarios:modificar'), autorizarPerfilPropioOPermiso('usuarios:modificar'), UsuarioController.update)
+router.delete('/:id', verificarToken, autorizarAlguno('perfil:eliminar', 'usuarios:eliminar'), autorizarPerfilPropioOPermiso('usuarios:eliminar'), UsuarioController.delete)
 
 export default router

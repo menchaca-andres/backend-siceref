17e562429659c26bf473d24604594bc1d1274fcb
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:49:45 -0400

    otorgando permisos por su codigo

--- src/modules/roles/role.routes.ts
+++ src/modules/roles/role.routes.ts
@@ -5,10 +5,10 @@ import { autorizar } from '../../middleware/roles.middleware'
 
 const router = Router()
 
-router.get('/', verificarToken, autorizar('Superadmin'), RoleController.getAll)
-router.get('/:id', verificarToken, autorizar('Superadmin'), RoleController.getById)
-router.post('/', verificarToken, autorizar('Superadmin'), RoleController.create)
-router.put('/:id', verificarToken, autorizar('Superadmin'), RoleController.update)
-router.delete('/:id', verificarToken, autorizar('Superadmin'), RoleController.delete)
+router.get('/', verificarToken, autorizar('roles:obtener'), RoleController.getAll)
+router.get('/:id', verificarToken, autorizar('roles:obtener'), RoleController.getById)
+router.post('/', verificarToken, autorizar('roles:crear'), RoleController.create)
+router.put('/:id', verificarToken, autorizar('roles:modificar'), RoleController.update)
+router.delete('/:id', verificarToken, autorizar('roles:eliminar'), RoleController.delete)
 
 export default router

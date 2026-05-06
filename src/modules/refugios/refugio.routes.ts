4d231c07c9320161859422175e285cf5291485d8
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:47:52 -0400

    otorgando permisos por su codigo

--- src/modules/refugios/refugio.routes.ts
+++ src/modules/refugios/refugio.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import { RefugioController } from './refugio.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
-import { autorizar } from '../../middleware/roles.middleware'
+import { autorizar, autorizarAlguno, autorizarRefugioPropioOPermiso } from '../../middleware/roles.middleware'
 
 const router = Router()
 
-router.get('/', verificarToken, autorizar('Superadmin'), RefugioController.getAll)
-router.get('/:id', verificarToken, autorizar('Superadmin'), RefugioController.getById)
-router.post('/', verificarToken, autorizar('Superadmin'), RefugioController.create)
-router.put('/:id', verificarToken, autorizar('Superadmin'), RefugioController.update)
-router.delete('/:id', verificarToken, autorizar('Superadmin'), RefugioController.delete)
+router.get('/', verificarToken, autorizar('refugios:obtener'), RefugioController.getAll)
+router.get('/:id', verificarToken, autorizarAlguno('refugio:obtener:propio', 'refugios:obtener'), autorizarRefugioPropioOPermiso('refugios:obtener'), RefugioController.getById)
+router.post('/', verificarToken, autorizar('refugios:crear'), RefugioController.create)
+router.put('/:id', verificarToken, autorizarAlguno('refugio:modificar:propio', 'refugios:modificar'), autorizarRefugioPropioOPermiso('refugios:modificar'), RefugioController.update)
+router.delete('/:id', verificarToken, autorizar('refugios:eliminar'), RefugioController.delete)
 
 export default router

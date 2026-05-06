dfaf60b0dd30740db91d8f3eb473b5a899b28ba4
menchaca-andres <4mrvro@gmail.com>
2026-05-06 04:09:22 -0400

    adaptando a PrismaClient

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,13 @@
-import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../generated/prisma/client'
 import { env } from './env'
 
-export const pool = new Pool({
+const adapter = new PrismaPg({
     host: env.db.host,
     port: env.db.port,
     database: env.db.name,
     user: env.db.user,
     password: env.db.password,
 })
+
+export const prisma = new PrismaClient({ adapter })

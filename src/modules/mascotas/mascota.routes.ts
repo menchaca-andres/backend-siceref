3284e9bff4aeb2025f9f5bb6417c230ea2b581b8
menchaca-andres <4mrvro@gmail.com>
2026-05-10 17:29:15 -0400

    importando middleware de carga de imagenes en las rutas crear y actualizar

--- src/modules/mascotas/mascota.routes.ts
+++ src/modules/mascotas/mascota.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { MascotaController } from './mascota.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
 import { autorizar } from '../../middleware/roles.middleware'
+import { uploadImage } from '../../middleware/upload.middleware'
 
 const router = Router()
 
 router.get('/', verificarToken, autorizar('mascotas:obtener'), MascotaController.getAll)
 router.get('/:id', verificarToken, autorizar('mascotas:obtener'), MascotaController.getById)
-router.post('/', verificarToken, autorizar('mascotas:crear'), MascotaController.create)
-router.put('/:id', verificarToken, autorizar('mascotas:modificar'), MascotaController.update)
+router.post('/', verificarToken, autorizar('mascotas:crear'), uploadImage.single('img_mascot'), MascotaController.create)
+router.put('/:id', verificarToken, autorizar('mascotas:modificar'), uploadImage.single('img_mascot'), MascotaController.update)
 router.delete('/:id', verificarToken, autorizar('mascotas:eliminar'), MascotaController.delete)
 
 export default router

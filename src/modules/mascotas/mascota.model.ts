9d831d7b0cc9211c367254b7ec6ca4d4d932117c
menchaca-andres <4mrvro@gmail.com>
2026-05-10 17:25:25 -0400

    actualizando createDto y updateDto

--- src/modules/mascotas/mascota.model.ts
+++ src/modules/mascotas/mascota.model.ts
@@ -18,13 +18,32 @@ export const MascotaModel = {
   },
 
   create: async (data: CreateMascotaDto) => {
-    return await prisma.mascotas.create({ data: { ...data, fechanac_mascot: toDate(data.fechanac_mascot)! } })
+    return await prisma.mascotas.create({
+      data: {
+        nom_mascot: data.nom_mascot,
+        img_mascot: data.img_mascot!,
+        fechanac_mascot: toDate(data.fechanac_mascot)!,
+        esteril_mascot: data.esteril_mascot === true || data.esteril_mascot === 'true',
+        sexo_mascot: data.sexo_mascot,
+        caract_mascot: data.caract_mascot,
+        id_raza: Number(data.id_raza),
+        id_ref: Number(data.id_ref),
+      },
+    })
   },
 
   update: async (id: number, data: UpdateMascotaDto) => {
     return await prisma.mascotas.update({
       where: { id_ani: id },
-      data: { ...data, fechanac_mascot: toDate(data.fechanac_mascot) },
+      data: {
+        ...data,
+        fechanac_mascot: toDate(data.fechanac_mascot),
+        esteril_mascot: data.esteril_mascot === undefined
+          ? undefined
+          : data.esteril_mascot === true || data.esteril_mascot === 'true',
+        id_raza: data.id_raza === undefined ? undefined : Number(data.id_raza),
+        id_ref: data.id_ref === undefined ? undefined : Number(data.id_ref),
+      },
     }).catch(() => null)
   },
 

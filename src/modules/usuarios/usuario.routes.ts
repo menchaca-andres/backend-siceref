85f1fafaf9f8ffac6df1975169c23c692cb7c8c3
menchaca-andres <4mrvro@gmail.com>
2026-05-28 16:01:31 -0400

    importando uploadImage para post y put

--- src/modules/usuarios/usuario.routes.ts
+++ src/modules/usuarios/usuario.routes.ts
@@ -2,14 +2,15 @@ import { Router } from 'express'
 import { UsuarioController } from './usuario.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
 import { autorizar, autorizarAlguno, autorizarPerfilPropioOPermiso } from '../../middleware/roles.middleware'
+import { uploadImage } from '../../middleware/upload.middleware'
 
 const router = Router()
 
 router.get('/', verificarToken, autorizar('usuarios:obtener'), UsuarioController.getAll)
 router.get('/trabajadores/mis-trabajadores', verificarToken, autorizar('trabajadores:obtener'), UsuarioController.getMyWorkers)
 router.get('/:id', verificarToken, autorizarAlguno('perfil:obtener', 'usuarios:obtener'), autorizarPerfilPropioOPermiso('usuarios:obtener'), UsuarioController.getById)
-router.post('/', verificarToken, autorizar('usuarios:crear'), UsuarioController.create)
-router.put('/:id', verificarToken, autorizarAlguno('perfil:modificar', 'usuarios:modificar'), autorizarPerfilPropioOPermiso('usuarios:modificar'), UsuarioController.update)
+router.post('/', verificarToken, autorizar('usuarios:crear'), uploadImage.single('img_usu'), UsuarioController.create)
+router.put('/:id', verificarToken, autorizarAlguno('perfil:modificar', 'usuarios:modificar'), autorizarPerfilPropioOPermiso('usuarios:modificar'), uploadImage.single('img_usu'), UsuarioController.update)
 router.delete('/:id', verificarToken, autorizarAlguno('perfil:eliminar', 'usuarios:eliminar'), autorizarPerfilPropioOPermiso('usuarios:eliminar'), UsuarioController.delete)
 
 export default router

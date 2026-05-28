36871a9fcd5b29a7a3a6fbda08a6ca125ac8b97c
menchaca-andres <4mrvro@gmail.com>
2026-05-28 15:40:56 -0400

    mov. upload para exportar a varios files

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -7,3 +7,13 @@ cloudinary.config({
 })
 
 export { cloudinary }
+
+export const uploadImageToCloudinary = async (file: Express.Multer.File, folder: string) => {
+  const base64 = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`
+  const result = await cloudinary.uploader.upload(base64, {
+    folder,
+    resource_type: 'image',
+  })
+
+  return result.secure_url
+}

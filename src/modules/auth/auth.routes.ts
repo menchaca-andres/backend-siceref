1c9c7b534b4c036b44b00707ee3b8e01a638c99a
menchaca-andres <4mrvro@gmail.com>
2026-05-28 15:46:30 -0400

    importando uploadImage

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { AuthController } from './auth.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
 import { autorizar } from '../../middleware/roles.middleware'
+import { uploadImage } from '../../middleware/upload.middleware'
 
 const router = Router()
 
 router.post('/login', AuthController.login)
-router.post('/register', AuthController.register)
-router.post('/register/worker', verificarToken, autorizar('trabajadores:crear'), AuthController.registerWorker)
-router.post('/register/superadmin', verificarToken, autorizar('admins-sistema:crear'), AuthController.registerSuperadmin)
-router.post('/register/admin-refugio', verificarToken, autorizar('admins-refugio:crear'), AuthController.registerAdminRefugio)
+router.post('/register', uploadImage.single('img_usu'), AuthController.register)
+router.post('/register/worker', verificarToken, autorizar('trabajadores:crear'), uploadImage.single('img_usu'), AuthController.registerWorker)
+router.post('/register/superadmin', verificarToken, autorizar('admins-sistema:crear'), uploadImage.single('img_usu'), AuthController.registerSuperadmin)
+router.post('/register/admin-refugio', verificarToken, autorizar('admins-refugio:crear'), uploadImage.single('img_usu'), AuthController.registerAdminRefugio)
 
 export default router

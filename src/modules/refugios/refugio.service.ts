daeedd1fe6fbc13596cfb5e8204b46cbd3f69270
menchaca-andres <4mrvro@gmail.com>
2026-05-28 15:53:29 -0400

    ag. img_ref en create y update, importanto uploadImageToCloudinary

--- src/modules/refugios/refugio.service.ts
+++ src/modules/refugios/refugio.service.ts
@@ -1,5 +1,6 @@
 import { RefugioModel } from './refugio.model'
 import { CreateRefugioDto, UpdateRefugioDto } from './refugio.types'
+import { uploadImageToCloudinary } from '../../config/cloudinary'
 
 export const RefugioService = {
     getAll: async () => await RefugioModel.findAll(),
@@ -10,14 +11,18 @@ export const RefugioService = {
         return refugio
     },
 
-    create: async (data: CreateRefugioDto) => {
+    create: async (data: CreateRefugioDto, file?: Express.Multer.File) => {
         const existe = await RefugioModel.findByEmail(data.email_ref)
         if (existe) throw new Error('El correo ya está registrado')
-        return await RefugioModel.create(data)
+
+        const img_ref = file ? await uploadImageToCloudinary(file, 'refugios') : data.img_ref
+
+        return await RefugioModel.create({ ...data, img_ref })
     },
 
-    update: async (id: number, data: UpdateRefugioDto) => {
-        const refugio = await RefugioModel.update(id, data)
+    update: async (id: number, data: UpdateRefugioDto, file?: Express.Multer.File) => {
+        const img_ref = file ? await uploadImageToCloudinary(file, 'refugios') : data.img_ref
+        const refugio = await RefugioModel.update(id, { ...data, img_ref })
         if (!refugio) throw new Error('Refugio no encontrado')
         return refugio
     },

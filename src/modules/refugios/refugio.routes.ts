de9ec4a0ea4f314b7434b326a19e8d522e92ae86
menchaca-andres <4mrvro@gmail.com>
2026-05-28 15:56:47 -0400

    importando uploadImage para post y put

--- src/modules/refugios/refugio.routes.ts
+++ src/modules/refugios/refugio.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { RefugioController } from './refugio.controller'
 import { verificarToken } from '../../middleware/auth.middleware'
 import { autorizar, autorizarAlguno, autorizarRefugioPropioOPermiso } from '../../middleware/roles.middleware'
+import { uploadImage } from '../../middleware/upload.middleware'
 
 const router = Router()
 
 router.get('/', verificarToken, autorizar('refugios:obtener'), RefugioController.getAll)
 router.get('/:id', verificarToken, autorizarAlguno('refugio:obtener:propio', 'refugios:obtener'), autorizarRefugioPropioOPermiso('refugios:obtener'), RefugioController.getById)
-router.post('/', verificarToken, autorizar('refugios:crear'), RefugioController.create)
-router.put('/:id', verificarToken, autorizarAlguno('refugio:modificar:propio', 'refugios:modificar'), autorizarRefugioPropioOPermiso('refugios:modificar'), RefugioController.update)
+router.post('/', verificarToken, autorizar('refugios:crear'), uploadImage.single('img_ref'), RefugioController.create)
+router.put('/:id', verificarToken, autorizarAlguno('refugio:modificar:propio', 'refugios:modificar'), autorizarRefugioPropioOPermiso('refugios:modificar'), uploadImage.single('img_ref'), RefugioController.update)
 router.delete('/:id', verificarToken, autorizar('refugios:eliminar'), RefugioController.delete)
 
 export default router

478d22a4fd99b981d2a91bd2a2c0d24791ca62f1
menchaca-andres <4mrvro@gmail.com>
2026-05-28 15:58:45 -0400

    ag. img_usu

--- src/modules/usuarios/usuario.types.ts
+++ src/modules/usuarios/usuario.types.ts
@@ -1,5 +1,6 @@
 export interface Usuario {
     id_usu: number
+    img_usu: string | null
     nom_usu: string
     apell_usu: string
     fecnac_usu: Date | string
@@ -10,23 +11,25 @@ export interface Usuario {
 }
 
 export interface CreateUsuarioDto {
+    img_usu?: string
     nom_usu: string
     apell_usu: string
     fecnac_usu: Date
     numcel_usu: string
     email_usu: string
     pass_usu: string
-    id_rol: number
-    id_ref?: number | null
+    id_rol: number | string
+    id_ref?: number | string | null
 }
 
 export interface UpdateUsuarioDto {
+    img_usu?: string
     nom_usu?: string
     apell_usu?: string
     fecnac_usu?: Date | string
     numcel_usu?: string
     email_usu?: string
     pass_usu?: string
-    id_rol?: number
-    id_ref?: number | null
+    id_rol?: number | string
+    id_ref?: number | string | null
 }
